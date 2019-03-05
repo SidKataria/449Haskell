51ba8ff9e5667e4889a75b1930c6ab7ff42c809d
Siddharth Kataria <32744185+SidKataria@users.noreply.github.com>
2019-03-05 14:48:09 -0700

    Add files via upload
    Still looping error

--- parsing.hs
+++ parsing.hs
@@ -14,18 +14,51 @@ trim = x . x
 --error function  
 errorFound :: Handle -> FilePath -> [Char] -> IO b0
 errorFound inHandle filename str = do
+  putStrLn "Error found"
   outHandle <- openFile filename WriteMode
   hPutStrLn outHandle (str)
   hClose outHandle
   hClose inHandle
   exitFailure
+
+  --Strings to int method
+intString :: (Integral b) => [Char] -> [b]
+intString "" = []
+intString other = (whichChar (other!!1)):[(whichChar (other!!3))]
+
+--Char to int returner
+whichChar :: (Integral b) => Char -> b
+whichChar x
+  | x == 'A' = 1
+  | x == 'B' = 2
+  | x == 'C' = 3
+  | x == 'D' = 4
+  | x == 'E' = 5
+  | x == 'F' = 6
+  | x == 'G' = 7
+  | x == 'H' = 8
+  | x == '1' = 1
+  | x == '2' = 2
+  | x == '3' = 3
+  | x == '4' = 4
+  | x == '5' = 5
+  | x == '6' = 6
+  | x == '7' = 7
+  | x == '8' = 8
+  | otherwise = -1
   
---nop else function for IO
-nop :: IO ()
-nop = sequence_ []
+--String in Integer
+whichString :: [Char] -> [Integer]
+whichString "" = []
+whichString other = [read other :: Integer]
+  
   
 --Main function
 main = do
+
+  --Test print
+  putStrLn "Just started main" 
+  
   args <- getArgs
   handle <- openFile (head args) ReadMode
   --outHandle <- openFile (last args) WriteMode
@@ -40,6 +73,9 @@ main = do
   hPutStrLn outHandle ("The file is: \n"++contents)
   hClose outHandle
   
+  --Test print
+  putStrLn "Contents gottem"
+  
   --Manipulating contents
   let input = lines contents
   
@@ -49,7 +85,7 @@ main = do
   let x = elemIndex "name:" givenLines
   let iName = case x of Nothing -> -1
                         Just a -> a
-
+ 
   --Finding Index Of  "forced partial assignment:" header
   let x = elemIndex "forced partial assignment:" givenLines
   let iFPA = case x of Nothing -> -1
@@ -75,8 +111,15 @@ main = do
   let iTNP = case x of Nothing -> -1
                        Just a -> a
 
-  --If Headers not found: ERROR
-  if ((iName == -1)||(iFPA == -1)||(iFM == -1)||(iTNT == -1)||(iMP == -1)||(iTNP == -1)) then (errorFound handle (last args) "Error while parsing input file") else (readFile (head args))
+  --Test print
+  putStrLn "Just before the if statement"
+  
+  --If Headers not found: ERROR (LOOPING)
+  if ((iName == -1)||(iFPA == -1)||(iFM == -1)||(iTNT == -1)||(iMP == -1)||(iTNP == -1)) 
+    then (errorFound handle (last args) "Error while parsing input file") else (putStrLn "")
+  
+  --Test print
+  putStrLn "Here now"
   
   --Strings data
   let sName = take (iFPA-(iName+1)) givenLines
@@ -86,6 +129,21 @@ main = do
   let sMP = take (iTNP-(iMP+1)) (drop (iMP+1) givenLines)
   let sTNP = drop (iTNP+1) givenLines
   
+  --Test print
+  putStrLn "Here now 2"
+  
+  --data structure for integers
+  let intFPA = [intString stringFPA | stringFPA <- sFPA, (length stringFPA) > 0]
+  
+  let intFM = [intString stringFM | stringFM <- sFM, (length stringFM) > 0]
+  
+  let intTNT = [intString stringTNT | stringTNT <- sTNT, (length stringTNT) > 0]
+  
+  let intMP = [intString stringMP | stringMP <- sMP, (length stringMP) > 0]
+  
+  let intTNP = [intString stringTNP | stringTNP <- sTNP, (length stringTNP) > 0]
+  
+  print intFPA
   
   --Closing handles
   --hClose outHandle
